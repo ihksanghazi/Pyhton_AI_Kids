c2a627b1073ea37e7f6f364dc269656cf8eaed1b
naskhi <nursandyihksan97@gmail.com>
2025-04-08 10:20:21 +0700

    meeting 10

--- meeting_9/main.py
+++ meeting_9/main.py
@@ -1,4 +1,7 @@
 import cv2
+from tracker import EuclideanDistTracker
+
+tracker = EuclideanDistTracker()
 
 cap = cv2.VideoCapture("highway.mp4")
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
@@ -9,11 +12,22 @@ while True:
     mask = object_detector.apply(roi)
     _,mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 100:
             x,y,w,h= cv2.boundingRect(cnt)
-            cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 2)
+            detections.append([x,y,w,h])
+            # cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 2)
+    # print(detections)
+    boxes_ids = tracker.update(detections)
+    print(boxes_ids)
+    for boxes_id in boxes_ids:
+        x,y,w,h,id = boxes_id
+        cv2.putText(roi, str(id), (x,y -15),
+        cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2 )
+        cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 2)
+    print(boxes_ids)
 
     # cv2.imshow("Frame", frame)
     # cv2.imshow("Mask", mask)
